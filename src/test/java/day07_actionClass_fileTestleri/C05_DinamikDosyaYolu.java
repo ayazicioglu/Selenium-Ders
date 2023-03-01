27413d030053da823f571e62f87241eba8373f38
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-01 11:06:08 +0300

    01.03.23

--- src/test/java/day07_actionClass_fileTestleri/C05_DinamikDosyaYolu.java
+++ src/test/java/day07_actionClass_fileTestleri/C05_DinamikDosyaYolu.java
@@ -1,2 +1,15 @@
-package day07_actionClass_fileTestleri;public class C05_DinamikDosyaYolu {
+package day07_actionClass_fileTestleri;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class C05_DinamikDosyaYolu {
+    @Test
+    public void test01(){
+        String dosyaYolu=System.getProperty("user.home")+"/Desktop/TheDelta.docx";
+        Assert.assertTrue(Files.exists(Path.of(dosyaYolu)));
+    }
 }
