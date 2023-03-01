d4179fc4e38d95c6176afa4703767b4875d4e4bd
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-01 11:07:00 +0300

    01.03.23

--- src/test/java/day07_actionClass_fileTestleri/C08_FileUploadTesti.java
+++ src/test/java/day07_actionClass_fileTestleri/C08_FileUploadTesti.java
@@ -1,2 +1,48 @@
-package day07_actionClass_fileTestleri;public class C08_FileUploadTesti {
+package day07_actionClass_fileTestleri;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.nio.file.WatchEvent;
+
+public class C08_FileUploadTesti extends TestBase {
+    @Test
+    public void test01(){
+        //2. https://the-internet.herokuapp.com/upload adresine gidelim
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+
+        //3. chooseFile butonuna basalim
+        //4. Yuklemek istediginiz dosyayi secelim.
+        //Downloads'a indirdigimiz logo.png dosyasini yukleyelim
+
+          /*
+            chooseFile butonuna basarsak
+            bilgisayarimizdan bir dosya secme islemi yapmamiz gerekir
+            Ancak selenium veya java bilgisayarimizdan dosya secme islemi yapamaz
+            Bunun yerine Selenium bize bir yontem gelistirmistir
+            chooseFile ile secmemiz gereken dosyanin dosya yolunu olusturup
+            olusturulan dosya yolunu sendKeys ile chooseFile butonuna gondermemiz yeterli olur
+         */
+
+        String dinamikDosyaYolu=System.getProperty("user.home")+"/Downloads/logo.png";
+        WebElement chooseFileButonu=driver.findElement(By.id("file-upload"));
+
+        chooseFileButonu.sendKeys(dinamikDosyaYolu);
+
+        //5. Upload butonuna basalim.
+        driver.findElement(By.id("file-submit")).click();
+
+        //6. “File Uploaded!” textinin goruntulendigini test edelim.
+        WebElement uploadedYaziElement=driver.findElement(By.tagName("h3"));
+        Assert.assertTrue(uploadedYaziElement.isDisplayed());
+
+        bekle(5);
+
+
+
+    }
 }
