9726bd1cf51207acfb0f3daf11dac51f001863a8
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-01 11:04:58 +0300

    01.03.23

--- src/test/java/day07_actionClass_fileTestleri/C01_MoveToElement.java
+++ src/test/java/day07_actionClass_fileTestleri/C01_MoveToElement.java
@@ -1,2 +1,37 @@
-package day07_actionClass_fileTestleri;public class C01_MoveToElement {
+package day07_actionClass_fileTestleri;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C01_MoveToElement extends TestBase {
+    //1- https://www.amazon.com/ adresine gidin
+    //2- Sag ust bolumde bulunan “Account & Lists” menusunun acilmasi icin
+    //mouse’u bu menunun ustune getirin
+    //3- “Create a list” butonuna basin
+    //4- Acilan sayfada “Your Lists” yazisi oldugunu test edin
+
+    @Test
+    public void test01(){
+        //1- https://www.amazon.com/ adresine gidin
+        driver.get("https://www.amazon.com/");
+
+        //2- Sag ust bolumde bulunan “Account & Lists” menusunun acilmasi icin mouse’u bu menunun ustune getirin
+        WebElement accountListElement=driver.findElement(By.xpath("//*[@id='nav-link-accountList']"));
+        Actions actions=new Actions(driver);
+        actions.moveToElement(accountListElement).perform();
+        bekle(2);
+
+        //3- “Create a list” butonuna basin
+        driver.findElement(By.xpath("//*[text()='Create a List']")).click();
+
+        //4- Acilan sayfada “Your Lists” yazisi oldugunu test edin
+        WebElement yourListElement=driver.findElement(By.id("my-lists-tab"));
+        Assert.assertTrue(yourListElement.isDisplayed());
+
+
+    }
 }
