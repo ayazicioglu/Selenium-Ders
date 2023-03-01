1db09ef91e540b713079990a08e1493ebf9246fa
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-01 11:05:21 +0300

    01.03.23

--- src/test/java/day07_actionClass_fileTestleri/C02_KeyboardActions.java
+++ src/test/java/day07_actionClass_fileTestleri/C02_KeyboardActions.java
@@ -1,2 +1,38 @@
-package day07_actionClass_fileTestleri;public class C02_KeyboardActions {
+package day07_actionClass_fileTestleri;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C02_KeyboardActions extends TestBase {
+    //1- Bir Class olusturalim KeyboardActions1
+    //2- https://www.amazon.com sayfasina gidelim
+    //3- Arama kutusuna actions method’larine kullanarak Samsung A71 yazdirin ve
+    //Enter’a basarak arama yaptirin
+    //4- aramanin gerceklestigini test edin
+
+    @Test
+    public void test01(){
+        //2- https://www.amazon.com sayfasina gidelim
+        driver.get("https://www.amazon.com");
+
+        //3- Arama kutusuna actions method’larine kullanarak Samsung A71 yazdirin ve
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+
+        Actions actions=new Actions(driver);
+        actions.click(aramaKutusu).keyDown(Keys.SHIFT).sendKeys("s").keyUp(Keys.SHIFT).
+                sendKeys("amsung ").keyDown(Keys.SHIFT).sendKeys("a").keyUp(Keys.SHIFT).
+                sendKeys("71").sendKeys(Keys.ENTER).perform();
+        bekle(3);
+
+        //4- aramanin gerceklestigini test edin
+        String actualAramaSonucYazi=driver.findElement(By.xpath("//span[@class='a-color-state a-text-bold']")).getText();
+        String expectedIcerik="Samsung A71";
+        Assert.assertTrue(actualAramaSonucYazi.contains(expectedIcerik));
+
+    }
 }
