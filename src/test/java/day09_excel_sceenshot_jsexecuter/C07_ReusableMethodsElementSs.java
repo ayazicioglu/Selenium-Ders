edb9fc0b590464694f93181242073fba8f9afdb5
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-04 20:26:58 +0300

    04.03.23

--- src/test/java/day09_excel_sceenshot_jsexecuter/C07_ReusableMethodsElementSs.java
+++ src/test/java/day09_excel_sceenshot_jsexecuter/C07_ReusableMethodsElementSs.java
@@ -1,2 +1,33 @@
-package day09_excel_sceenshot_jsexecuter;public class C07_ReusableMethodsElementSs {
+package day09_excel_sceenshot_jsexecuter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+import static utilities.TestBaseClass.driver;
+
+public class C07_ReusableMethodsElementSs extends TestBase {
+
+    @Test
+    public void test01(){
+        //amazon anasayfaya gidin
+        driver.get("https://www.amazon.com");
+
+        //Nutella icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+
+        //Sonuclarin Nutella icerdiğini test edin
+        WebElement sonucYaziElementi = driver.findElement(By.xpath("//h1[@class='a-size-base s-desktop-toolbar a-text-normal']"));
+        String expectedIcerik = "Nutella";
+        String actualSonucYazisi = sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
+
+        //ve rapora eklenmek icin sonuc yazi elementinin goruntusunu alın
+        ReusableMethods.webElementScreenshot(sonucYaziElementi);
+    }
 }
--- src/test/java/day09_excel_sceenshot_jsexecuter/C08_JavaScriptExecuterClick.java
+++ src/test/java/day09_excel_sceenshot_jsexecuter/C08_JavaScriptExecuterClick.java
@@ -1,2 +1,33 @@
-package day09_excel_sceenshot_jsexecuter;public class C08_JavaScriptExecuter {
+package day09_excel_sceenshot_jsexecuter;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C08_JavaScriptExecuterClick extends TestBase {
+    @Test
+    public void test01(){
+        //wisaquarter anasayfaya gidelim
+        driver.get("https://www.wisequarter.com");
+
+        //refresh diyerek reklamdan kurtul
+        driver.navigate().refresh();
+
+        //Java Test otomation tanitimina kadar asagı inin
+        WebElement otomasyonElement=driver.findElement(By.xpath("(//img[@alt='Automation Engineer'])[1]"));
+        bekle(2);
+        JavascriptExecutor jse= (JavascriptExecutor) driver;
+        bekle(2);
+        jse.executeScript("arguments[0].scrollIntoView();",otomasyonElement);
+
+        // ve resmi click yapın
+        jse.executeScript("arguments[0].click();",otomasyonElement);
+        bekle(2);
+
+        //ekrana "JUnit bitti" yazdirin
+        jse.executeScript("alert('JUnit Bitti');");
+        bekle(2);
+    }
 }
