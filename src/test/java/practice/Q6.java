e3a37da2f46792d2e4ae5e2f0905c6f5c71265cd
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-03-06 11:03:55 +0300

    06.03.23

--- src/test/java/practice/Q6.java
+++ src/test/java/practice/Q6.java
@@ -1,2 +1,47 @@
-package practice;public class Q6 {
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Q6 {
+
+    @BeforeClass
+    public static void setup(){
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void teardown(){
+        System.out.println("After Class");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("Before");
+    }
+
+    @After
+    public void after(){
+        System.out.println("After");
+    }
+
+    @Test
+    public void ahmet(){
+
+        System.out.println("Ahmet");
+    }
+
+    @Test
+    public void betul(){
+        System.out.println("Betul");
+    }
+
+    @Test
+    public void cengiz(){
+        System.out.println("Cengiz");
+    }
 }
